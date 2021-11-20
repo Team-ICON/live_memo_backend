c96adb41dffa9c0fd932a1e5f8d39a64abbea35d
AnSolomon <solomonivy@naver.com>
2021-11-21 02:36:31 +0900

    createMemo mod

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.oaxnc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.${process.env.DB_MIDDLE_URL}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     {   //options
         useNewUrlParser: true,
         useUnifiedTopology: true
