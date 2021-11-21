615005d63167cca45b9dc21045dc4c7529ac018a
SEO <plant00park21@gmail.com>
2021-11-21 17:57:04 +0900

    createMemo final

--- src/Router/memoRouter.js
+++ src/Router/memoRouter.js
@@ -15,10 +15,7 @@ import { isLoggedin } from "../Services/AuthService";
 
 const router = express.Router();
 
-// router.put("/createMemo", isLoggedin, createMemo);
-router.put("/createMemo", createMemo);
-
-
+router.put("/createMemo", isLoggedin, createMemo);
 router.post("/delete", isLoggedin, deleteMemo);
 router.post("/save", isLoggedin, saveMemo);
 router.post("/addbookmark", isLoggedin, addBookmark);
@@ -28,6 +25,7 @@ router.post("/addUser", isLoggedin, addUser);
 router.get("/getMemos", isLoggedin, showMemos); // main에 메모리스트 전체 뜨는 것
 router.get("/getMemo/:id", isLoggedin, viewMemo); // 하나의 메모 조회
 
+// router.put("/createMemo", createMemo);
 
 // router.post("/addUser", addUser);
 // router.get("/getMemo/:id", viewMemo); // 하나의 메모 조회
