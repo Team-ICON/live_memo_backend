db2869c16554fe89e4a99aac2195409e3921cf48
KsPsD <ui9621@naver.com>
2021-11-24 15:45:54 +0900

    merge

--- src/Services/AuthService.js
+++ src/Services/AuthService.js
@@ -14,8 +14,8 @@ passport.use(
     new GoogleStrategy(
         {
             // options for strategy
-            callbackURL: `https://livememo-backend.herokuapp.com/api/user/auth/google/callback/`,
-            // callbackURL: `http://localhost:4000/api/user/auth/google/callback/`,
+            // callbackURL: `https://livememo-backend.herokuapp.com/api/user/auth/google/callback/`,
+            callbackURL: `http://localhost:4000/api/user/auth/google/callback/`,
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         },
@@ -48,7 +48,8 @@ export const signToken = async (req, res) => {
             try {
                 // res.cookie('livememo-token', token, { httpOnly:false, secure: false });
                 console.log('token json send');
-                res.redirect(`https://livememo-frontend.web.app/token/${token}`);
+                res.cookie('livememo-token', token);
+                res.redirect(`https://localhost:3000/${token}`);
                 // return res.status(200).json({ token });
                 // req.session.livememo = token;
                 // res.cookie('livememo-token', token);
